943c3a86c7887d5fa272e2c0c43977a11db4151e
Tim Herrmann <therrma2@gmail.com>
2016-03-19 14:01:20 -0500

    renamed package, added localization launch file to /launch instead of launching direction from robot_localization

--- src/odomsync.py
+++ src/odomsync.py
@@ -82,6 +82,70 @@ def callback(data,args):
         bool = serv(offset2)
         print bool, "step 1"
 
+
+    if data.data == 99:
+        print "returned 99"
+        empty = Empty()
+        pubodom.publish(empty)
+
+        #### Send 0 pose to /set_pose topic to /zero odometry/filtered
+        newodom = PoseWithCovarianceStamped()
+        newodom.header.frame_id = 'odom'
+        newodom.header.stamp = rospy.Time.now()
+        newodom.pose.pose.position.x = 0.0
+        newodom.pose.pose.position.y = 0.0
+        newodom.pose.pose.position.z = 0.0
+        newodom.pose.pose.orientation.x = 0.0
+        newodom.pose.pose.orientation.y = 0.0
+        newodom.pose.pose.orientation.z = 0.0
+        newodom.pose.pose.orientation.w = 1.0
+        pubfiltered.publish(newodom)
+
+ #### get current transform from base_meas to odom_meas
+
+
+        try:
+            (trans,rot) = listener.lookupTransform('/odom_meas','/base_meas',rospy.Time(0))
+            print trans
+            print rot
+        except:
+            print "transform not found"
+        offset = Odometry()
+        xsave = offset.pose.pose.position.x = -trans[0]
+        ysave = offset.pose.pose.position.y = -trans[1]
+        zsave = offset.pose.pose.position.z = -trans[2]
+
+        x2 = offset.pose.pose.orientation.x = 0.0
+        y2 = offset.pose.pose.orientation.y = 0.0
+        z2 = offset.pose.pose.orientation.z = 0.0
+        w2 = offset.pose.pose.orientation.w = 0.0
+        (roll,pitch,yaw) = tf.transformations.euler_from_quaternion((x2,y2,z2,w2))
+
+        #### use current transform to set odom_offset
+        try:
+            (trans,rot) = listener.lookupTransform('/base_meas','/odom_meas',rospy.Time(0))
+            print trans
+            print rot
+        except:
+            print "transform not found"
+
+        offset2 = Odometry()
+
+        offset2.pose.pose.position.x = xsave
+        offset2.pose.pose.position.y = ysave
+        offset2.pose.pose.position.z = zsave
+
+        x2 = offset2.pose.pose.orientation.x = 0
+        y2 = offset2.pose.pose.orientation.y = 0
+        z2 = offset2.pose.pose.orientation.z = 0
+        w2 = offset2.pose.pose.orientation.w = 0
+
+        bool = serv(offset2)
+        print bool, "step 1"
+
+
+        
+
     if data.data == 10:
         print "returned 10"
         zero = Odometry()
